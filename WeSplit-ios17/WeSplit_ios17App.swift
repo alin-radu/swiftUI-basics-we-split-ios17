da640b2706cc9e9e2de2484ee69f5c396eb6a558
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 20:54:54 +0200

    Initial Commit

---
+++ WeSplit-ios17/WeSplit_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_ios17App.swift
+//  WeSplit-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
